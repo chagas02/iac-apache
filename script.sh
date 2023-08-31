9dd4d61e017b084dff9e911402ca9ae668c66329
Ubuntu <ubuntu@ip-172-31-41-188.ec2.internal>
2023-08-31 01:17:40 +0000

    Apache v.1.0

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "atualizando SO"
+sudo apt update
+sudo apt upgrade -y
+
+echo "instalando o apache2"
+sudo apt install apache2 -y
+
+echo "Instalando o unzip"
+sudo apt install unzip -y
+
+echo "baixando a aplicação"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+
+echo "copiando os arquivos"
+
+sudo cp -R * /var/www/html/
+
