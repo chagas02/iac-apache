6cb35739bf97fa3f50080e3513e5579cb570be0e
chagas02 <90938920+chagas02@users.noreply.github.com>
2023-08-30 22:21:39 -0300

    corrigi o nome da pasta
    faltou o main no nome da pasta.

--- script.sh
+++ script.sh
@@ -14,7 +14,7 @@ echo "baixando a aplicação"
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 
 echo "copiando os arquivos"
 
